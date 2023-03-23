fe0be7cb4320700574384ceb307ea3da9f373411
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-03-23 16:52:59 +0700

    Membuat cout elemen array dengan menambah kondisi

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -8,5 +8,17 @@ void input() {
 	while (true) {
 		cout << "Masukkan Jumlah Data Pada Array : ";
 		cin >> n;
+		if (n <= 20) {
+			break;
+		}
+		else {
+			cout << "\nArray yang anda masukkan maksimal 20 elemen. \n";
+		}
+		cout << endl;
+		cout << "=====================" << endl;
+		cout << "Masukkan Elemen Array" << endl;
+		cout << "=====================" << endl;
+
+
 	}
 }
