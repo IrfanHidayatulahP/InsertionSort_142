7d8869f48caa0650e702337da480497495e58392
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-03-23 22:24:16 +0700

    Fix Update

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -6,43 +6,52 @@ int n;
 
 void input() {
 	while (true) {
-		cout << "Masukkan Jumlah Data Pada Array : ";
+		cout << "Masukan Jumlah Data pada Array : ";
 		cin >> n;
-		if (n <= 20) {
+
+		if (n <= 20)
+		{
 			break;
 		}
-		else {
-			cout << "\nArray yang anda masukkan maksimal 20 elemen. \n";
+		else
+		{
+			cout << "\nArray yang anda masukkan maksimal 20 element. \n";
 		}
-		cout << endl;
-		cout << "=====================" << endl;
-		cout << "Masukkan Elemen Array" << endl;
-		cout << "=====================" << endl;
+	}
+	cout << endl;
+	cout << "==========================" << endl;
+	cout << "Masukkan Element Array" << endl;
+	cout << "==========================" << endl;
 
-		for (int i = 0; i < n; i++) {
-			cout << "Data ke-" << (i + 1) << ": ";
-			cin >> arr[i];
-		}
+	for (int i = 0; i < n; i++)
+	{
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
 	}
 };
 
-void insertionsort() {
+
+void insertionsort() 
+{
 	int temp;
 	int j;
 
-	for (int i = 1; i < n; i++) {
+	for (int i = 1; i < n; i++) 
+	{
 		temp = arr[i];
 		j = i - 1;
 
-		while (j >= 0 && arr[j] > temp); {
-			arr[j + 1] = temp;
+		while (j >= 0 && arr[j] > temp) 
+		{
+			arr[j + 1] - arr[j];
 			j--;
 		}
 		arr[j + 1] = temp;
 
 		cout << endl;
 		cout << "\nPass " << i << ": ";
-		for (int k = 0; k < n; k++) {
+		for (int k = 0; k < n; k++) 
+		{
 			cout << arr[k] << " ";
 		}
 	}
