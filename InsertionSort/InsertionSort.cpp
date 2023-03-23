7436dc8233d1039659e2325eebfc47fe25040dd6
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-03-23 16:56:42 +0700

    membuat perulangan dengan for untuk menyimoan data i

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -19,6 +19,10 @@ void input() {
 		cout << "Masukkan Elemen Array" << endl;
 		cout << "=====================" << endl;
 
-
+		for (int i = 0; i < n; i++) {
+			cout << "Data ke-" << (i + 1) << ": ";
+			cin >> arr[i];
+		}
 	}
-}
+};
+
