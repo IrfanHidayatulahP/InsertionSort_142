718952dec35c2d2b2bce3162ed612e62f65f2106
IrfanHidayatulahP <irfanhputra400@gmail.com>
2023-03-23 17:14:01 +0700

    menambah kondisi dan cout array variable j

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -55,4 +55,8 @@ void display() {
 	cout << "Element Array yang telah tersusun" << endl;
 	cout << "=================================" << endl;
 
+	for (int j = 0; j < n; j++) {
+		cout << arr[j] << endl;
+	}
+	cout << endl;
 }
